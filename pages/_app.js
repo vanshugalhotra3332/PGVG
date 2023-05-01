c97bace7aa190f7d9adfc28d869afef31d59d2bb
Vanshu Galhotra <vanshugalhotra3332@gmail.com>
2023-05-01 18:43:54 +0530

    style (Loading bar): added a top loading bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,16 @@
+// components
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
+import LoadingBar from "react-top-loading-bar";
+
 //css imports
 import "@/styles/globals.css";
 import "@/styles/slug.css";
+// react
 import { SessionProvider } from "next-auth/react";
+import { useState } from "react";
+import { useRouter } from "next/router";
+// redux
 import { store } from "../store/store";
 import { Provider } from "react-redux";
 import { useEffect } from "react";
@@ -23,10 +30,36 @@ export default function App({
       window.removeEventListener("resize", handleResize);
     };
   }, []);
+  const [progress, setProgress] = useState(0);
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChangeStart = () => {
+      setProgress(40);
+    };
+
+    const handleRouteChangeComplete = () => {
+      setProgress(100);
+    };
+
+    router.events.on("routeChangeStart", handleRouteChangeStart);
+    router.events.on("routeChangeComplete", handleRouteChangeComplete);
+
+    return () => {
+      router.events.off("routeChangeStart", handleRouteChangeStart);
+      router.events.off("routeChangeComplete", handleRouteChangeComplete);
+    };
+  }, [router, setProgress]);
   return (
     <>
       <SessionProvider session={session}>
         <Provider store={store}>
+          <LoadingBar
+            color="#2563EB"
+            progress={progress}
+            waitingTime={400}
+            onLoaderFinished={() => setProgress(0)}
+          />
           <Navbar />
           <Component {...pageProps} />
           <Footer />
